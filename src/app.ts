eca7d002926fc915e013bbda59dfd0f1d071ec59
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-19 10:45:53 -0300

    refactor

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import EventEmitter from 'events';
+export const eventApp = new EventEmitter()
+
+
+
+
+
+
---
+++ src/utils/format-data.ts
@@ -0,0 +1,25 @@
+import { stateData } from "../types/types"
+
+export const formatData = (data: string): stateData => {
+  const search = data.split('>')
+  const response = {
+    population: 0,
+    signup_population: 0,
+    vaccinated_population: 0
+  }
+
+  search.forEach((arr: string, index: number) => {
+    if (arr.includes('POPULAÇÃO DO RN')) {
+      response.population = parseInt(search[index + 2].replace('</h1', '').replace(/\./g, ''))
+    }
+    if (arr.includes('PESSOAS CADASTRADAS')) {
+      response.signup_population = parseInt(search[index + 2].replace('</h1', '').replace(/\./g, ''))
+    }
+    if (arr.includes('PESSOAS VACINADAS')) {
+      response.vaccinated_population = parseInt(search[index + 2].replace('</h1', '').replace(/\./g, ''))
+    }
+  })
+  return response
+}
+
+
