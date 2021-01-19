97d67daa6ef3f6c52886f1aa705a3668df963722
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-19 15:02:13 -0300

    fix fetch

--- src/api/fetch-data-web.ts
+++ src/api/fetch-data-web.ts
@@ -7,9 +7,15 @@ export const fetchData = async (): Promise<stateData> => {
   try {
     const { data } = await axios.get('https://maisvacina.saude.rn.gov.br/transparencia/mapa-cidadaos.json')
     // const formatedData = formatData(data);
-    const formatedData = reduceData(data.data)
+    // const formatedData = reduceData(data.data)
+    const response: stateData = {
+      population: data.total_populacao_rn,
+      signup_population: data.total_cidadaos_cadastrados,
+      vaccinated_population: data.total_cidadaos_vacinados
+    }
+    console.log(response)
 
-    return formatedData;
+    return response;
   } catch (_) {
     return await fetchData()
 
