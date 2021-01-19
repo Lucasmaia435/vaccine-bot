2137d87b5f64cddc5ccf38038485a7fa07cbdbd0
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-19 11:05:08 -0300

    fix: add event listen

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import EventEmitter from 'events';
+import { updateVaccineDataController } from './controller/update-vaccine-data';
 export const eventApp = new EventEmitter()
+
+
+eventApp.on('update', updateVaccineDataController)
 
 
 
